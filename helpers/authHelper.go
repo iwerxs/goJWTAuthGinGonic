a965194fc23ec3a7041717281aa7280074edd1ed
mlaane <goobric@gmail.com>
2024-06-09 11:55:10 +0100

    check user credentials and profile

--- helpers/authHelper.go
+++ helpers/authHelper.go
@@ -1,1 +1,33 @@
 package helpers
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CheckUserType(c *gin.Context, role string) (err error){
+	userType := c.GetString("user_type")
+	err = nil
+	if userType != role {
+		err = errors.New("unathorized to access this resource")
+		return err
+	}
+	return err
+}
+
+func MatchUserTypeToUid(c *gin.Context, userId string) (err error) {
+	userType := c.GetString("user_type")
+	uid := c.GetString("uid")
+	err = nil
+
+	if userType == "USER" && uid != userId {
+		err = errors.New("unauthorized to access this resource")
+		return err
+	}
+	// add CAdmin, PAdmin via if conditionals
+
+	// helper function
+	err = CheckUserType(c, userType)
+	return err
+}
