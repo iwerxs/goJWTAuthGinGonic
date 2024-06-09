810f2ac51c53da9df70a484de2e909323a09a85f
mlaane <goobric@gmail.com>
2024-06-09 11:30:18 +0100

    setup functions defined in userRouter

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,1 +1,23 @@
 package controllers
+
+// "fmt"
+// "log"
+// "time"
+// "os"
+// "context"
+// "strconv"
+// "github.com/gin-gonic/gin"
+// "github.com/go-playground/validator/v10"
+
+// setup functions defined in userRouter
+func HashPassword()
+
+func VerifyPassword()
+
+func Register()
+
+func Login()
+
+func GetUsers()
+
+func GetUser()
