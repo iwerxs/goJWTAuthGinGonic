2ce83ae8c9fdd08ce6aa88af5a01e6cc74bb5a32
mlaane <goobric@gmail.com>
2024-06-09 10:16:38 +0100

    setup api routes for project

--- main.go
+++ main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	routes "goJWTAuthGinGonic/routes"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+	port := os.Getenv("PORT")
+
+	if port==""{
+		port="8080"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	// two api
+	router.GET("/api-1", func(c *gin.Context){
+		c.JSON(200, gin.H{"success":"access granted api1"})
+	})
+	router.GET("/api-2", func(c *gin.Context){
+		c.JSON(200, gin.H{"success":"access granted api2"})
+	})
+	router.Run(":" + port)
+}
